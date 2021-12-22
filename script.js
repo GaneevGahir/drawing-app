53c23508eb88abaf7b0356665752040926e73d94
GaneevGahir <95885033+GaneevGahir@users.noreply.github.com>
2021-12-22 18:14:26 +0300

    checkpoint 3

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var canvas = document.getElementById("canvas");
 var increaseBtn = document.getElementById("increase");
 var decreaseBtn = document.getElementById("decrease");
-
 var sizeEl = document.getElementById("size");
 var colorEl = document.getElementById("color");
 var clearEl = document.getElementById("clear");
@@ -81,49 +80,4 @@ clearEl.addEventListener('click', function () {
 function updateSizeOnScreen() {
     sizeEl.innerText = size;
 }
-});
-
-function drawCircle() {
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.fillStyle = color;
-    ctx.fill();
-}
-
-function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = size * 2;
-    ctx.stroke();
-}
-
-increaseBtn.addEventListener("click", function () {
-    size += 5;
-    if (size > 50) {
-        size = 50;
-    }
-    updateSizeOnScreen();
-})
-
-
-decreaseBtn.addEventListener("click", function () {
-    size -= 5;
-    if (size < 5) {
-        size = 5;
-    }
-    updateSizeOnScreen();
-})
-
-colorEl.addEventListener("change", function (e) {
-    color = e.target.value;
-})
-
-clearEl.addEventListener('click', function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-})
-
-function updateSizeOnScreen() {
-    sizeEl.innerText = size;
 }
