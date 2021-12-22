180d3f0a853a80b962b2c80bfa18ee1309f61570
GaneevGahir <95885033+GaneevGahir@users.noreply.github.com>
2021-12-22 19:39:57 +0300

    checkpoint 6

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var canvas = document.getElementById("canvas");
 var increaseBtn = document.getElementById("increase");
 var decreaseBtn = document.getElementById("decrease");
-
 var sizeEl = document.getElementById("size");
 var colorEl = document.getElementById("color");
 var clearEl = document.getElementById("clear");
@@ -81,4 +80,3 @@ clearEl.addEventListener('click', function () {
 function updateSizeOnScreen() {
     sizeEl.innerText = size;
 }
-
